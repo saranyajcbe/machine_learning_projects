b0ba03d05a85bb5e2421733f13b71e54b03d33aa
saranya <saran.suje@gmail.com>
2025-10-31 11:44:38 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT='-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirments=[]
+    with open(file_path) as file_obj:
+        requirments=file_obj.readlines()
+        requirments=[req.replace("\n","") for req in requirments ]
+
+        if HYPEN_E_DOT in requirments:
+            requirments.remove(HYPEN_E_DOT)
+
+setup(
+name='machine_learning_ptojects',
+version='0.0.1',
+author='Saranya',
+author_email='saran.suje@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+)
